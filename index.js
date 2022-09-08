60c19c3ba8b66931eb63a790f0eff11e78a17503
uzairkhan-12 <pk.uzikhan@gmail.com>
2022-09-08 12:39:40 +0500

    deploy

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
 const mongoose = require('mongoose')
 const { MONGOURI } = require('./key')
 var cors = require('cors')
 app.use(cors())
 mongoose.connect(MONGOURI)
-
+const port = 5000 || process.env.PORT
 app.use(express.json())
 require('./models/user')
 require('./models/skills')
@@ -22,6 +23,6 @@ mongoose.connection.on('error',(err)=>{
     console.log({error : err})
 })
 
-app.listen('5000',() => {
+app.listen(port,() => {
     console.log('app is running')
 })
